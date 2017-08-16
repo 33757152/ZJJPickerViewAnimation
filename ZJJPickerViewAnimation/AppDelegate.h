c0274adb4427cded18004be24c914d4c355da97e
张锦江 <zhangjinjiang@zhangjinjiangdeMacBook-Pro.local>
2017-08-16 15:07:31 +0800

    Initial Commit

---
+++ ZJJPickerViewAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJJPickerViewAnimation
+//
+//  Created by 张锦江 on 2017/8/16.
+//  Copyright © 2017年 ZJJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZJJPickerViewAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJJPickerViewAnimation
+//
+//  Created by 张锦江 on 2017/8/16.
+//  Copyright © 2017年 ZJJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
